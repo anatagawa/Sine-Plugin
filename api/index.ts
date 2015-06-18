46ad45e58396f1b76712862d9e4ba5b0b114c71f
anatagawa <anata@wanadoo.fr>
2015-06-18 20:56:43 +0200

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,7 @@
 import * as fs from "fs";
 
 SupAPI.registerPlugin("typescript", "Sine", {
-  code: fs.readFileSync(`${__dirname}/Sup.Sine.ts.txt`, { encoding: "utf8" }),
-  defs: fs.readFileSync(`${__dirname}/Sup.Sine.d.ts.txt`, { encoding: "utf8" }),
-  exposeActorComponent: { propertyName: "sine", className: "Sup.Sine" }
+	code: fs.readFileSync(`${__dirname}/Sup.Sine.ts.txt`, { encoding: "utf8" }),
+	defs: fs.readFileSync(`${__dirname}/Sup.Sine.d.ts.txt`, { encoding: "utf8" }),
+	exposeActorComponent: { propertyName: "sine", className: "Sup.Sine" }
 });
